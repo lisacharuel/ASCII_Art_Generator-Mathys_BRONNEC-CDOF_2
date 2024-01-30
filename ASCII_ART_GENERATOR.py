553c81c2b70059a918c5a1326117c93628672dde
Red0pium <Mathys.bronnec@gmail.com>
2024-01-30 11:23:03 +0100

    text modification

--- ASCII_ART_GENERATOR.py
+++ ASCII_ART_GENERATOR.py
@@ -1,4 +1,6 @@
 from ASCII_ART_DICT import Ascii_letters
-test = input("Enter a string: ")
 
-print('\n'.join([Ascii_letters[letter.capitalize()] for letter in test])) 
+
+test = input("Enter a string to transform: ")
+
+print('\n'.join([Ascii_letters[letter.capitalize()] for letter in test]))
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='{ASCII_Art_Generator-Mathys_BRONNEC-CDOF_2}',
+    version='1.0',
+    author='{Red0pium}',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
